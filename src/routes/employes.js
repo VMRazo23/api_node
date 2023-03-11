f569227cde00f1890dff86ddee4b3d7a7ffc01c0
Victor Razo <94213407+VMRazo23@users.noreply.github.com>
2023-03-10 19:44:05 -0600

    firs comit

---
+++ src/routes/employes.js
@@ -0,0 +1,13 @@
+import {Router} from 'express'
+import {getEmployes, createEmployes, updateEmploye, deleteEmploye, getEmploye} from '../controllers/employes.controller.js'
+
+const router = Router();
+
+router.get('/employes', getEmployes)
+router.get('/employes/:id', getEmploye)
+router.post('/employes', createEmployes)
+router.patch('/employes/:id', updateEmploye)
+router.delete('/employes/:id', deleteEmploye)
+
+export default router
+
