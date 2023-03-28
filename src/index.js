465ada44c5c538242234e227db17a254837b5bcf
Victor Razo <94213407+VMRazo23@users.noreply.github.com>
2023-03-28 12:44:44 -0600

    prueba api

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/api',employes)
-app.use('/api/',products)
+app.use('/.netlify/functions/api/',employes)
+app.use('/.netlify/functions/api/',products)
 
 app.use((req, res, next) => {
     res.status(404).json({
